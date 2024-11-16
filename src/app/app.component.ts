c6c5f9494aadf52e58fd51cdb5b179a07ab14693
priyankareddymodugula <priyankareddy1708@gmail.com>
2024-11-16 14:55:07 +0530

    fixed http client issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [DashboardComponent]
 })
 export class AppComponent {
   title = 'ABC-Dashboard';
